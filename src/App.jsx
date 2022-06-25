e6ebf1bff76536cce3b1c47852001f6ab731a29c
deVamshi <dev.vamshidhar@gmail.com>
2022-06-25 20:30:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1 className="title">deVamshi</h1>
+        <hr />
+        <p className="about">
+          a full stack developer with a passion... <strong> CRAP! </strong> I'll
+          just get to the point. I build software using the below technologies.
+        </p>
+        <p className="skills">ReactJs | Flutter | NodeJs</p>
+        {/* <button className="more-button">that's it?</button> */}
+        {/* <hr />
+        <p className="about">
+          I am nothing special, of this I am sure. I am a common man with common
+          thoughts and I've led a common life. There are no monuments dedicated
+          to me and my name will soon be forgotten, but I've loved another with
+          all my heart and soul, and to me, this has always been enough
+        </p>
+        <p className="skills">- Nicholas Sparks</p> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
